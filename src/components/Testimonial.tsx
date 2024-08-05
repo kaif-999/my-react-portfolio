d8a1c5664086ad1b880c0be0dfd188584a58a374
kaif-999 <idrisikaif8427@gmail.com>
2024-08-05 16:52:06 +0530

    changes

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -221,7 +221,7 @@ const Testimonial = () => {
                     <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
                       <div>
                         <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                          Travel Mobile App Design.
+                          Buil Website for my hospital staff Managment System
                         </h3>
                         <p className="text-base text-gray-400 mt-3">
                             Mar 4, 2015 - Aug 30, 2021 
