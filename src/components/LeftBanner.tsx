89e7128713616bd39cb329507db502f154a37acb
kaif-999 <idrisikaif8427@gmail.com>
2025-01-26 15:57:09 +0530

    adding new component file for testing

--- src/components/LeftBanner.tsx
+++ src/components/LeftBanner.tsx
@@ -14,7 +14,7 @@ const LeftBanner = () => {
   return (
     <FadeIn className="w-full lgl:w-1/2 flex flex-col gap-20">
       <div className="flex flex-col gap-5">
-        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h4 className=" text-lg font-normal">WELCOME TO MY Website</h4>
         <h1 className="text-6xl font-bold text-white">
           Hi, I'm <span className="text-designColor capitalize">Mohammed Kaif</span>
         </h1>
